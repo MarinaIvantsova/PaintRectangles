720b955573996317af2ce548dabe7fa3e3940f3c
Marina Ivantsova <marineivantsova@yandex.ru>
2023-02-21 16:40:45 +0700

    Refactoring

--- index.js
+++ index.js
@@ -5,35 +5,30 @@ const windowWidth = document.documentElement.clientWidth;
 
 const windowSquare = windowHeight * windowWidth;
 
-const getSumResidents =  function(locations) {
-  let residentsArray = [];
-  locations.forEach((elem) => {
-    residentsArray.push(elem.residents.length)
-  });
-  return residentsArray;
+function getArrayOfResidents(locations) {
+  return locations.map(elem => elem.residents.length);
 }
 
 function getOneResidentSquare(locations) {
-  const residentsSum = getSumResidents(locations).reduce(
+  const residentsSum = getArrayOfResidents(locations).reduce(
     (accumulator, currentValue) => accumulator + currentValue,
     0);
 
     return (windowSquare / residentsSum);
 }
 
-function createRectangle(quantityResidents, oneResidentSquare) {
+function createRectangle(quantityOfResidents, oneResidentSquare) {
   const div = document.createElement('div');
 
-  div.classList.add("mystyle");
+  div.classList.add('mystyle');
   div.style.width = `${windowWidth}px`;
-  div.style.height = `${quantityResidents * (oneResidentSquare / windowWidth)}px`;
+  div.style.height = `${quantityOfResidents * (oneResidentSquare / windowWidth)}px`;
   document.body.appendChild(div);
 }
 
 fetch(url)
   .then(response => response.json())
-  .then(results => ({
-    oneResidentSquare: getOneResidentSquare(results.results),
-    quantityResidents: getSumResidents(results.results)
-  }))
-  .then(res => res.quantityResidents.forEach(elem => createRectangle(elem, res.oneResidentSquare)));
+  .then(results => {
+    return getArrayOfResidents(results.results)
+    .forEach(quantityOfResidents => createRectangle(quantityOfResidents, getOneResidentSquare(results.results))
+  )});
